7d92bfb3ad37bed3661129971153157897380308
chenanddom <772571631@qq.com>
2018-01-01 11:41:52 +0800

    添加服务代理和配置中心

--- MicroService_Zuul/MicroService_Zuul_Client/src/main/java/com/gzuul/ZuulApplication.java
+++ MicroService_Zuul/MicroService_Zuul_Client/src/main/java/com/gzuul/ZuulApplication.java
@@ -1,7 +1,9 @@
 package com.gzuul;
 
+import com.netflix.discovery.EurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -10,5 +12,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZuulApplication {
 public static void main(String[] args){
     SpringApplication.run(ZuulApplication.class,args);
+//    DiscoveryClient
 }
 }
--- MicroService_Zuul/MicroService_Zuul_Server/src/main/java/com/gzuul/ZuulApplication.java
+++ MicroService_Zuul/MicroService_Zuul_Server/src/main/java/com/gzuul/ZuulApplication.java
@@ -1,5 +1,6 @@
 package com.gzuul;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
