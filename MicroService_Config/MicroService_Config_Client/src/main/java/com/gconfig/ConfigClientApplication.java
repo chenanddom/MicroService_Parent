9a658f75e9f03426098c7cdee23598f6b9b32830
chenanddom <772571631@qq.com>
2018-01-07 11:50:01 +0800

    修改客户端配置，实现客户端的配置从git获取，去掉pom内多余的依赖，使得整个项目统一风格

--- MicroService_Config/MicroService_Config_Client/src/main/java/com/gconfig/ConfigClientApplication.java
+++ MicroService_Config/MicroService_Config_Client/src/main/java/com/gconfig/ConfigClientApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
     public static void main(String[] args){
         SpringApplication.run(ConfigClientApplication.class,args);
