998b88072c9ead25dbaa298fbabc5172d8ccab12
chenanddom <772571631@qq.com>
2018-01-07 10:27:30 +0800

    提交springCloud自动获取配置文件的项目

--- MicroService_Config/MicroService_Config_Client/src/main/java/com/gconfig/ConfigClientApplication.java
+++ MicroService_Config/MicroService_Config_Client/src/main/java/com/gconfig/ConfigClientApplication.java
@@ -1,15 +1,18 @@
 package com.gconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 //@EnableDiscoveryClient
-@EnableConfigServer
-public class ConfigApplication {
+public class ConfigClientApplication {
     public static void main(String[] args){
-        SpringApplication.run(ConfigApplication.class,args);
+        SpringApplication.run(ConfigClientApplication.class,args);
     }
 }
