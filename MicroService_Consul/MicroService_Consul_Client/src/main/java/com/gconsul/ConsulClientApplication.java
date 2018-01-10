65f5280303cf47a3b90d2f93c031c4c4225e4bbd
chenanddom <772571631@qq.com>
2018-01-10 12:59:32 +0800

    添加服务发现Consul

---
+++ MicroService_Consul/MicroService_Consul_Client/src/main/java/com/gconsul/ConsulClientApplication.java
@@ -0,0 +1,23 @@
+package com.gconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ConsulClientApplication {
+
+    @RequestMapping("/getInformation")
+    public String home() {
+        return "this is information";
+    }
+
+
+    public static void main(String[] args){
+        SpringApplication.run(ConsulClientApplication.class,args);
+    }
+}
