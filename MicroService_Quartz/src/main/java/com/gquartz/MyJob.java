d99742074cc99774cb32e3132ab70e14b8d47bb4
chenanddom <772571631@qq.com>
2018-01-11 07:56:40 +0800

    添加定制任务quartz

---
+++ MicroService_Quartz/src/main/java/com/gquartz/MyJob.java
@@ -0,0 +1,16 @@
+package com.gquartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+        System.out.println(sdf.format(new Date()));
+    }
+}
